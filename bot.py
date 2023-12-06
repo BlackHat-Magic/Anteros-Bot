89f97e78ed09f9016aacf60bbd1039320193dc55
Luke Henderson <personal@lukeh.us>
2023-12-05 19:56:59 -0800

    fixed typo

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ async def on_message(message):
     view = MessageButtons(convo)
     searched_ids = []
     while len("".join(message_.content for message_ in messages)) > 4096:
-        if(messages[0].content.startswith("# System Message\n\n" and messages[0].author == client.user)):
+        if(messages[0].content.startswith("# System Message\n\n") and messages[0].author == client.user):
            messages.pop[1]
            continue
         messages.pop(0)
