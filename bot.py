495bd891c85d698368fb4a046d0a0f99f15813af
Luke Henderson <personal@lukeh.us>
2024-10-18 05:12:20 -0700

    syntax

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ async def on_message(message):
         messages=convo
     )
     response = response.choices[0].message.content
-    split_response = [response[i:i+1900 for i in range(0, len(string), 1900)]]
+    split_response = [response[i:i+1900] for i in range(0, len(string), 1900)]
     for chunk in split_response:
         latest = await message.channel.send(chunk)
     await latest.edit(view=view)
